d8a9f6707acdd0a570b326b9f078cfbaa0da7fc5
VitorAguiar <vitor.aguiar@me.com>
2018-01-25 10:50:20 -0200

    fix bug: was not selecting column subject

--- 3-kallisto_pipeline/3-process_imgt_quants_round1.R
+++ 3-kallisto_pipeline/3-process_imgt_quants_round1.R
@@ -9,7 +9,7 @@ samples <- readLines("../data/sample_ids.txt")
 imgt_quants <- read_tsv("./quantifications_1/imgt_quants.tsv") %>%
     mutate(locus = imgt_to_gname(target_id),
 	   gene_id = gname_to_gid(locus)) %>%
-    select(locus, gene_id, allele = target_id, est_counts, tpm)
+    select(subject, locus, gene_id, allele = target_id, est_counts, tpm)
 
 missing_files <- samples[! samples %in% imgt_quants$subject]
 
