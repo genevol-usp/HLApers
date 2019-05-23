859e349efa0a9d37e1d8ac4138d68d9b560a7c9d
VitorAguiar <vitor.aguiar@me.com>
2019-05-23 16:21:00 -0300

    update quant mode

--- script/run_quant.sh
+++ script/run_quant.sh
@@ -11,43 +11,38 @@ index=${outPrefix}_index
 sample_gencode=${outPrefix}_gencode.fa
 fq1=${outPrefix}_1.fq.gz
 fq2=${outPrefix}_2.fq.gz
+bampers=${outPrefix}_Aligned.out.bam
+out=${outPrefix}_quant
 
 samtools sort -n $bam |\
-    samtools fastq -1 $fq1 -2 $fq2 -
+    samtools fastq -N -1 $fq1 -2 $fq2 -
 
+mkdir -p $index
 
-#mkdir -p $indexDIR
-#
-#cat $gencodeNoIMGT $sample_hla > $sample_gencode
-#
-#STAR --runThreadN $CPUS --runMode genomeGenerate --genomeDir $indexDIR\
-#    --genomeFastaFiles $sample_gencode\
-#    --genomeChrBinNbits 11 --genomeSAindexNbases 13\
-#    --outFileNamePrefix ${indexDIR}_
-#
-#
-#STAR --runMode alignReads --runThreadN $CPUS --genomeDir $indexDIR\
-#    --readFilesIn $fq1 $fq2 --readFilesCommand zcat\
-#    --outFilterMismatchNmax 999\
-#    --outFilterMismatchNoverReadLmax 0.04\
-#    --outFilterMultimapScoreRange 1\
-#    --outFilterMultimapNmax 150\
-#    --winAnchorMultimapNmax 300\
-#    --alignIntronMax 0\
-#    --alignEndsType Local\
-#    --outSAMunmapped Within KeepPairs\
-#    --outSAMprimaryFlag AllBestScore\
-#    --outSAMtype BAM Unsorted\
-#    --outFileNamePrefix ${prefix}_
-#
-#bam=${outPrefix}Aligned.out.bam
-#outQuant=./quantifications
-#out=$outQuant/$sample
-#
-#if [ -d "$out" ]; then
-#    rmdir $out
-#fi
-#
-#$salmon quant -t $sample_fa -l IU -a $bam -o $out -p $CPUS --seqBias --gcBias
-#
-#rm -r $indexDIR ${indexDIR}_Log.out $sample_fa $outPrefix* 
+cat $gencodeNoHLA $samplehla > $sample_gencode
+
+STAR --runThreadN $cpus --runMode genomeGenerate --genomeDir $index\
+    --genomeFastaFiles $sample_gencode\
+    --genomeChrBinNbits 11 --genomeSAindexNbases 13\
+    --outFileNamePrefix ${index}_
+
+STAR --runMode alignReads --runThreadN $cpus --genomeDir $index\
+    --readFilesIn $fq1 $fq2 --readFilesCommand zcat\
+    --outFilterMismatchNmax 999\
+    --outFilterMismatchNoverReadLmax 0.04\
+    --outFilterMultimapScoreRange 1\
+    --outFilterMultimapNmax 150\
+    --winAnchorMultimapNmax 300\
+    --alignIntronMax 0\
+    --alignEndsType Local\
+    --outSAMunmapped Within KeepPairs\
+    --outSAMprimaryFlag AllBestScore\
+    --outSAMtype BAM Unsorted\
+    --outFileNamePrefix ${outPrefix}_
+
+salmon quant -t $sample_gencode -l A -a $bampers -o $out -p $cpus \
+    --seqBias --gcBias --posBias
+
+mv ${outPrefix}_*Log.* ${outPrefix}_logs/
+
+rm -r $bampers $fq1 $fq2 $index $sample_gencode ${outPrefix}_SJ* 
